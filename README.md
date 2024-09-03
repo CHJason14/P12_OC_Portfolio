25d9eef3f4005872c0958a2321c93a1bbe625e8e
Jason Chiampi <jason.chiampi@hotmail.fr>
2024-09-03 11:40:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+### Setup
+ 
+Install required dependencies
+
+<pre>yarn install</pre>
+
+
+Start the server
+
+<pre>yarn start</pre>
+
---
+++ src/content_option.js
@@ -0,0 +1,107 @@
+const logotext = "JASON";
+const meta = {
+    title: "Jason Chiampi",
+    description: "Je me présente, Jason, j'ai 27 ans et je suis développeur Front End au nord de Paris",
+};
+
+const introdata = {
+    title: "Je m'appelle Jason Chiampi",
+    animated: {
+        first: "J'aime développer ",
+        second: "Je réalise de jolies applications",
+        third: "Je travail sérieusement",
+    },
+    description: "Prêt à écouter tous vos projets, pour les réaliser en fonction de vos besoins et de vos envies",
+    your_img_url: "https://i.ibb.co/z7f7jsT/image-principale.webp",
+};
+
+const dataabout = {
+    title: "Un peu sur moi",
+    aboutme: "Je m'appelle Jason, j'ai 27 ans, je suis jeune développeur Front End. Passionné depuis mes 13 ans par le développement j'ai choisit une reconversion professionnel pour m'orienter dans un domaine qui me convient.",
+};
+const worktimeline = [{
+        jobtitle: "Mastering Front End - From Scratch",
+        where: "En ligne",
+        date: "2023",
+    },
+    {
+        jobtitle: "Intégrateur Web - OpenClassRoom",
+        where: "En ligne",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "SASS",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+];
+
+const services = [{
+        title: "Création Site Web",
+        description: "Création de site web en HTML/CSS ou React en suivant vos demandes.",
+    },
+    {
+        title: "Référencement Web",
+        description: "Optimisation du référencement de votre application Web.",
+    },
+    {
+        title: "Mise à jour et maintenance",
+        description: "Mise à jour et maintenance de vos applications web déjà existantes.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/yXPW45p/OhMyfood.jpg",
+        description: "Application de réservation de menu dans un restaurant",
+        link: "https://chjason14.github.io/P4_OC_Ohmyfood/index.html",
+    },
+    {
+        img: "https://i.ibb.co/m8nVhwq/Printit.jpg",
+        description: "Application d'une entreprise d'impression en boutique et en ligne",
+        link: "https://chjason14.github.io/P5_OC_Printiit/",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jason.chiampi.dev@gmail.com",
+    YOUR_FONE: "07.81.91.96.29",
+    description: "Voici mes différents contacts, il est plus simple de communiquer par mail pour toutes questions ou demandes, si vous souhaitez par téléphone je suis disponible de 8h à 19h du lundi au samedi.",
+    YOUR_SERVICE_ID: "service_od4aoxq",
+    YOUR_TEMPLATE_ID: "template_iz0bpnf",
+    YOUR_USER_ID: "X8SIjgVBEIiV9YJpO",
+};
+
+const socialprofils = {
+    github: "https://github.com/CHJason14",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
